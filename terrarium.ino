c556a29287c0f195c42fcdd98db4c3ed9a4a3a42
Nathan Scott <s222196837@deakin.edu.au>
2022-03-28 06:48:47 +1100

    Make the threshold daylight value a Particle variable

--- terrarium.ino
+++ terrarium.ino
@@ -1,11 +1,11 @@
-// The following line is optional, but recommended in most firmware. 
+// The following line is optional, but recommended in most firmware.
 // It allows your code to run before the cloud is connected.
 SYSTEM_THREAD(ENABLED);
 
 // This uses the USB serial port for debugging logs.
 SerialLogHandler logHandler;
 
-// This is where your LED is plugged in. The other side goes to a resistor 
+// This is where your LED is plugged in. The other side goes to a resistor
 // connected to GND.
 const pin_t LED_PIN = D6;
 
@@ -13,9 +13,10 @@ const pin_t LED_PIN = D6;
 const pin_t SENSOR_PIN = A0;
 
 // This is the light threshold for determining sufficient daylight
-const int MIN_AMBIENT_LIGHT = 70;
+const int MIN_AMBIENT_LIGHT = 75;
+int threshold = MIN_AMBIENT_LIGHT;
 
-// Here we are declaring the integer variable analogvalue, which we will 
+// Here we are declaring the integer variable analogvalue, which we will
 // use later to store the value of the photoresistor or phototransistor.
 int analogvalue;
 
@@ -34,30 +35,31 @@ int ledToggle(int enable) {
 }
 
 void setup() {
-    // First, declare all of our pins. This lets our device know which ones 
-    // will be used for outputting voltage, and which ones will read 
+    // First, declare all of our pins. This lets our device know which ones
+    // will be used for outputting voltage, and which ones will read
     // incoming voltage.
     pinMode(LED_PIN, OUTPUT); // Our LED pin is output (lighting up the LED)
     digitalWrite(LED_PIN, HIGH);
 
-    // We are going to declare a Particle.variable() here so that we can 
-    // access the value of the photosensor from the cloud.
+    // We are going to declare Particle.variable() here so that we can
+    // access these values relating to the photosensor from the cloud.
     Particle.variable("analogvalue", analogvalue);
+    Particle.variable("threshold", threshold);
     Particle.variable("daytime", daytime);
 }
 
 void loop() {
-    // Check to see what the value of the photoresistor or phototransistor is 
+    // Check to see what the value of the photoresistor or phototransistor is
     // and store it in the int variable analogvalue
     analogvalue = analogRead(SENSOR_PIN);
 
-    // This prints the value to the USB debugging serial port (for optional 
+    // This prints the value to the USB debugging serial port (for optional
     // debugging purposes)
     Log.info("analogvalue=%d", analogvalue);
 
     // Implement the "sufficient daylight" sensing logic - if the value of
     // ambient light sensor is above a threshold we determine its daytime.
-    daytime = (analogvalue > MIN_AMBIENT_LIGHT);
+    daytime = (analogvalue > threshold);
 
     // Switch the LED on/off and send notification to the IFTTT service if
     // we determine the daylight state has just changed.  Note: switch the
